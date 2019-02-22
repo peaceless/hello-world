c003685440e5942a104ff1d2a05b069c47321bab
peaceless <1103243851@qq.com>
2019-02-22 17:11:38 +0800

    add commit to Client and server

--- Client.cpp
+++ Client.cpp
@@ -1,3 +1,4 @@
+//Client.cpp impliment a Client link to server
 #include <iostream>
 #include <cstdlib>
 #include <memory.h>
@@ -6,10 +7,10 @@
 #include <sys/socket.h>
 class Client {
 private:
-    char receive[100];
-    char sent[100];
-    int serv_sock;
-    sockaddr_in serv_addr;
+    char receive[100]; //receive info from net
+    char sent[100];    //sent to net(server)
+    int serv_sock;     //store socket id
+    sockaddr_in serv_addr;//struct of socket within msg of link
 public:
     Client(std::string server_ip = "127.0.0.1");
     void connecting();
@@ -18,22 +19,29 @@ public:
 Client::Client(std::string server_ip)
 {
     const char * ip = server_ip.c_str();
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(ip);
-    serv_addr.sin_port = htons(1234);
+    memset(&serv_addr,0,sizeof(serv_addr));//將服務器初始化，默認為0
+    //set server's confirguration
+    serv_addr.sin_family = AF_INET; //use IPv4
+    serv_addr.sin_addr.s_addr = inet_addr(ip); //set server ip address
+    serv_addr.sin_port = htons(1234); //set server listen port
 }
-
+// implement link between client and server
 void Client::connecting()
 {
+//initialize sent and receive buffer with 0
     memset(&sent,0,sizeof(sent));
     memset(&receive,0,sizeof(receive));
     while (1) {
         int sock;
+//create a socket,return socket descriptor with success or minus number with failed
         if ((sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)) < 0) {
             std::cout << "Creat socket fail!" << std::endl;
             exit(0);
         }
+//before connect or listen,use bind() to make socket bind to a port
+//in this example,use temporary port allocate by operation system
+//use socket and provide server's msg,
+//link to server return 0 with success
         if(connect(sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
             std::cout << "Can't connect!" << std::endl;
             exit(0);
--- Server.cpp
+++ Server.cpp
@@ -1,14 +1,17 @@
+//Server.cpp --create a server to wait for Client's link requirements
 #include <iostream>
 #include <cstring> //use bzero
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
+//declration class Server
 class Server {
 private:
     int serv_sock;
     char receive[100];
+//store configuration of server
     sockaddr_in serv_addr;
+//store configuration of client
     sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
     pid_t childpid;
@@ -19,6 +22,7 @@ public:
 
 void Server::init()
 {
+    //                 family, type,       protocal
     serv_sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
     bzero(&serv_addr,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -28,11 +32,14 @@ void Server::init()
 
 void Server::wait()
 {
+    //bind socket with address
     bind(serv_sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+    //listen the port and define max amount of link
     listen(serv_sock,20);
 
     while(1){
         clnt_addr_size = sizeof(clnt_addr);
+        //accept return a new socket's hanlder,use this communicate with client
         int clnt_sock = accept(serv_sock,(struct sockaddr *)&clnt_addr,&clnt_addr_size);
         if ((childpid = fork()) == 0){
             read(clnt_sock,receive,100);
