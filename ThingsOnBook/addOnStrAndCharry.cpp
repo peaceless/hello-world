29a3dfc0659c94e9c649a37cc9666c377d883497
peaceless <1103243851@qq.com>
2020-08-15 18:48:21 +0800

    clean dir tree

--- ThingsOnBook/addOnStrAndCharry.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main()
-{
-    std::string str_address;
-    char a[15];
-    std::cout << "str is :" << &str_address 
-              << "char array :" << sizeof(a) << std::endl;
-}
--- makefile
+++
@@ -1,5 +0,0 @@
-all:server client
-server:
-	g++ -std=c++11 Server.cpp -o server
-client:
-	g++ -std=c++11 Client.cpp -o client
